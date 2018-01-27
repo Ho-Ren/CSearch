5de004d8b8f5872a43db9f8a15acb8cd42381f4f
horenkan50 <horenkan50@gmail.com>
2018-01-26 23:40:19 -0500

    First commit

---
+++ Search/Search.c
@@ -0,0 +1,16 @@
+/*Ho-Ren Kang horenk 21397985*/
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+    char file[20];
+    int target = 0;
+  
+    
+    strcpy(file, argv[1]);
+    target = atoi(argv[2]);
+    
+    printf("%d", num);
+    return 0;
+}
